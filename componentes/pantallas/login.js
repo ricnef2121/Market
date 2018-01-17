547ce19792995fa6cf0dd0798b606cba6bc9683a
ricnef2121 <ricnef2121@gmail.com>
2018-01-17 11:25:23 -0600

    se agrego la carpeta componentes, dentro de ella la carpeta pantallas, y dentro de esta ultima el archivo login.js

---
+++ componentes/pantallas/login.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import{
+    Platform,
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+
+class Login extends Component{
+    render(){
+        return(
+            <View>
+                <Text>
+                Login
+                </Text>
+            </View>
+        );
+    }
+}
+
+
+export default Login;
